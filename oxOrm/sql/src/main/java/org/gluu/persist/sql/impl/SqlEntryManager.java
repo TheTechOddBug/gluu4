80c933e413a553732841b83d7bb715921648ad97
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2025-05-07 22:14:16 +0300

    feat(fido2): fix fido2 device registration
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>

--- oxOrm/sql/src/main/java/org/gluu/persist/sql/impl/SqlEntryManager.java
+++ oxOrm/sql/src/main/java/org/gluu/persist/sql/impl/SqlEntryManager.java
@@ -474,7 +474,7 @@ public class SqlEntryManager extends BaseEntryManager<SqlOperationService> imple
 		try {
 			convertedExpression = toSqlFilter(key, getBaseObjectClass(entryClass, objectClasses), searchFilter, propertiesAnnotationsMap);
 		} catch (SearchException ex) {
-            throw new EntryPersistenceException(String.format("Failed to convert filter '%s' to expression", searchFilter));
+            throw new EntryPersistenceException(String.format("Failed to convert filter '%s' to expression", searchFilter), ex);
 		}
 
         PagedResult<EntryData> searchResult = null;
